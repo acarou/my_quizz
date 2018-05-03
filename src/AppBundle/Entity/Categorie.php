0e3cf9b74d1dcb8653dd0768b1860240c7a98f25
quentin <quentin1.sommer@epitech.eu>
2018-05-03 14:00:12 +0200

    prepare to ez admin

---
+++ src/AppBundle/Entity/Categorie.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Categorie
+ *
+ * @ORM\Table(name="categorie")
+ * @ORM\Entity
+ */
+class Categorie
+{
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=100, nullable=false)
+     */
+    private $name;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+
+}
